5c50f2bfd53b638321951de82e28d3f43bc9f624
Vlad Mocanu <vmocanu@deloittece.com>
2022-02-10 13:18:00 +0200

    other commit

--- force-app/test/jest-mocks/c/displayPanel.js
+++ force-app/test/jest-mocks/c/displayPanel.js
@@ -4,4 +4,13 @@ export default class DisplayPanel extends LightningElement {
   @api errors;
   @api notes;
   // any other implementation you may want to expose here
+
+  const bla = 's' + 'bb';
+  let strLang = 'javscript';
+
+  const arr = [1,2,3,4,5];
+
+  arr.sort((a, b) => a - b);
+  console.log(arr);
+
 }
